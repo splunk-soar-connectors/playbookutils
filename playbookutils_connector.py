83f937ebc179dc2c4665d39e35aecadacfd7976b
Brandan Neises <bneises@splunk.ccom>
2022-04-15 10:06:24 -0500

    Fixed long lines, requests timeout/verify, sys.exit, and added contributors

--- playbookutils_connector.py
+++ playbookutils_connector.py
@@ -534,6 +534,7 @@ class PlaybookUtilsConnector(BaseConnector):
 
 
 def main():
+    import sys
     import pudb
     import argparse
 
@@ -562,7 +563,8 @@ def main():
             login_url = PlaybookUtilsConnector._get_phantom_base_url() + '/login'
 
             print("Accessing the Login page")
-            r = requests.get(login_url, verify=False)
+            verify = False
+            r = requests.get(login_url, verify=verify, timeout=30)
             csrftoken = r.cookies['csrftoken']
 
             data = dict()
@@ -575,11 +577,11 @@ def main():
             headers['Referer'] = login_url
 
             print("Logging into Platform to get the session id")
-            r2 = requests.post(login_url, verify=False, data=data, headers=headers)
+            r2 = requests.post(login_url, verify=verify, data=data, headers=headers, timeout=30)
             session_id = r2.cookies['sessionid']
         except Exception as e:
             print("Unable to get session id from the platform. Error: " + str(e))
-            exit(1)
+            sys.exit(1)
 
     with open(args.input_test_json) as f:
         in_json = f.read()
@@ -596,7 +598,7 @@ def main():
         ret_val = connector._handle_action(json.dumps(in_json), None)
         print(json.dumps(json.loads(ret_val), indent=4))
 
-    exit(0)
+    sys.exit(0)
 
 
 if __name__ == '__main__':
